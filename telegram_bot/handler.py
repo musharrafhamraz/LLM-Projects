dcbd16555fed4c3b9e123fd35bf0976ca59bc809
musharrafhamraz <musharrafhamraz02@gmail.com>
2024-12-17 12:17:54 +0500

    message handler whether the bot has to respond in a group or a private chat.

---
+++ telegram_bot/handler.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from constants import USERNAME
+from groq_client import handle_response
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    if message_type == 'group':
+        if USERNAME in text:
+            new_text: str = text.replace(USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = handle_response(text)
+
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
